91e58e0607ce4ab0d5c8e770b9e0f52f009a281c
Shabeer M <msg2shabeer@gmail.com>
2016-10-16 14:43:49 +0530

    add route /users/ - to get all users

--- api/index.php
+++ api/index.php
@@ -16,6 +16,23 @@ $app->get('/',function(){
 	echo "Welcome to Complaint Management API system";
 });
 
+// get all users
+$app->get('/users/',function() use($app){
+	include_once 'config/db.php';
+	$users = array();
+	foreach ($complaint->users() as $user) {
+		$users[] = array(
+			'id'			=> $user['id'],
+			'user_id'		=> $user['user_id'],
+			'name'			=> $user['name'],
+			'user_type_id'	=> $user['user_type_id']
+		);
+	}
+	$app->response()->header("Content-Type","application/json");
+	echo json_encode($users);
+});
+
+
 // run the application
 $app->run();
 ?>
