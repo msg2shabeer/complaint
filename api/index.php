be2c875dcd4c796f1653b754fc4ac230f6290ad8
Shabeer M <msg2shabeer@gmail.com>
2016-10-16 14:47:45 +0530

    add route /user/:id - to get single user with curresponding id

--- api/index.php
+++ api/index.php
@@ -32,6 +32,31 @@ $app->get('/users/',function() use($app){
 	echo json_encode($users);
 });
 
+// get single user
+$app->get('/user/:id',function($uid) use($app){
+	include_once 'config/db.php';
+	$user = array();
+	foreach ($complaint->users("id = ?",$uid) as $suser) {
+		$user[] = array(
+			'id'			=> $suser['id'],
+			'user_id'		=> $suser['user_id'],
+			'name'			=> $suser['name'],
+			'user_type_id'	=> $user['user_type_id']
+		);
+		$app->response()->header("Content-Type","application/json");
+		echo json_encode($user);
+	}
+});
+
+// put a single user
+
+// get all complaints
+
+// get a complaint by its id
+
+// get complaint by customer id
+
+// put a complaint
 
 // run the application
 $app->run();
